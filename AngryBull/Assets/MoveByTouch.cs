a17d3ce63a328bba83f048bbec55e7115a4b9d52
paulandrei98 <petrisorpaul98@gmail.com>
2020-10-22 16:38:46 +0300

    Scene1 - Level Design/Player Movement/Collisions

---
+++ AngryBull/Assets/MoveByTouch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       for (int i=0; i<Input.touchCount; i++)
+        {
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+        }
+    }
+}
