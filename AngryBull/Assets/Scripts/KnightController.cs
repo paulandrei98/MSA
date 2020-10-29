c0d26426ec6ba3e9bc38cdb98f605f0c967ef042
paulandrei98 <petrisorpaul98@gmail.com>
2020-10-29 16:29:47 +0200

    Passive Character -> Movement/Idle/Attack Animations + key controlls

---
+++ AngryBull/Assets/Scripts/KnightController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : MonoBehaviour
+{
+
+    float speed = 4;
+    float rotSpeed = 80;
+    float gravity = 8;
+    float rot = 0f;
+    Vector3 moveDir = Vector3.zero;
+    CharacterController controller;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        GetInput();
+    }
+
+    void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                if(anim.GetBool("attacking") == true)
+                {
+                    return;
+                }
+                else if(anim.GetBool("attacking") == false)
+                {
+                    anim.SetBool("running", true);
+                    anim.SetInteger("condition", 1);
+                    moveDir = new Vector3(0, 0, 1);
+                    moveDir *= speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetBool("running", false);
+                anim.SetInteger("condition", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+        }
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+
+
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+
+    void GetInput()
+    {
+        if(controller.isGrounded)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(anim.GetBool("running") == true)
+                {
+                    anim.SetBool("running", false);
+                    anim.SetInteger("condition", 0);
+                }
+                if(anim.GetBool("running") == false)
+                {
+                    Attacking();
+                }
+
+            }
+        }
+    }
+
+    void Attacking()
+    {
+
+        StartCoroutine(AttackRoutine());
+
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        anim.SetBool("attacking", true);
+        anim.SetInteger("condition", 2);
+        yield return new WaitForSeconds(1);
+        anim.SetInteger("condition", 0);
+        anim.SetBool("attacking", false);
+    }
+}
